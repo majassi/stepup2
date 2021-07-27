81d509640baabccd85b483293119a9c67e2592a2
majassi <46198752+majassi@users.noreply.github.com>
2021-07-27 15:00:51 +0200

    Add files via upload

---
+++ precache-manifest.21824d7ff094748d73f5d82bfae3c0fa.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1219d05bb7f97a1478d2f6f550751b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c5101e6343fbc1c33dd",
+    "url": "/static/css/main.de31fb63.chunk.css"
+  },
+  {
+    "revision": "3c886d3d738d8d1b87d4",
+    "url": "/static/js/2.da32f4dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.da32f4dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c5101e6343fbc1c33dd",
+    "url": "/static/js/main.dad29958.chunk.js"
+  },
+  {
+    "revision": "a636d80f4c60ac69ceae",
+    "url": "/static/js/runtime-main.adf7435c.js"
+  },
+  {
+    "revision": "1d481ed8cb546db3f47e9b7c4fe1c4e9",
+    "url": "/static/media/bgVideo.1d481ed8.mp4"
+  },
+  {
+    "revision": "5a7f1ee745de4cb803ec339e828ecb9a",
+    "url": "/static/media/classFifth.5a7f1ee7.jpg"
+  },
+  {
+    "revision": "ec2cfd8758600274596d9707332b0346",
+    "url": "/static/media/classFour.ec2cfd87.jpg"
+  },
+  {
+    "revision": "1caf779152a05c7b79b5104fa5bc346e",
+    "url": "/static/media/classOne.1caf7791.jpg"
+  },
+  {
+    "revision": "fb0c991e134904fc35db1dd22bc39437",
+    "url": "/static/media/classThree.fb0c991e.jpg"
+  },
+  {
+    "revision": "3b842ce173709e0f8a7d0efb1dc6dd86",
+    "url": "/static/media/classTwo.3b842ce1.jpg"
+  },
+  {
+    "revision": "31cb05f3e1c4697812b2104049d21c55",
+    "url": "/static/media/contactBg.31cb05f3.jpg"
+  },
+  {
+    "revision": "0aed62fe5a7e7eb94df7043c526c5bf8",
+    "url": "/static/media/gymOverlayBg.0aed62fe.jpg"
+  },
+  {
+    "revision": "02e364823438fedfc552123239968bf6",
+    "url": "/static/media/scheduleBg.02e36482.jpg"
+  },
+  {
+    "revision": "7626932cf4dffdb44e2dd7d6326a586f",
+    "url": "/static/media/trainerOne.7626932c.jpg"
+  },
+  {
+    "revision": "3a251761f14633575a8de2e6451d171b",
+    "url": "/static/media/trainerThree.3a251761.jpg"
+  },
+  {
+    "revision": "22349595bd0437b122d6a9896119d2a4",
+    "url": "/static/media/trainerTwo.22349595.jpg"
+  }
+]);
